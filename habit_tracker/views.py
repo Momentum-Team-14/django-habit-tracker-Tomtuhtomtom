745b1ed3e85e2a458d4a4403ab686d7640948155
Thomas Morris <tomchrismorris@yahoo.com>
2022-09-11 22:07:33 -0400

    test link for using date in urls working, shows all records for every habit, need to filter and still need to fix constraint bug

--- habit_tracker/views.py
+++ habit_tracker/views.py
@@ -102,48 +102,31 @@ def record_detail(request, pk):
 @login_required
 def test_detail(request, pk, year=None, month=None, day=None):
     habit = get_object_or_404(Habit, pk=pk)
-    record = Record.objects.all()
+    records = Record.objects.all().order_by('-entry_date')
     if year is None:
         date_for_record = datetime.date.today()
     else:
         date_for_record = datetime.date(year, month, day)
+    next_day = date_for_record + datetime.timedelta(days=1)
+    prev_day = date_for_record + datetime.timedelta(days=-1)
     # year = record.entry_date
     # month = record.entry_date.month
     # day = record.entry_date.day
 
-    return render(request, 'habit_tracker/test_detail.html', {"habit": habit, "record": record, "date_for_record": date_for_record, "year": year, "month": month, "day": day})
-    # record = get_object_or_404(Record, pk=pk)
-    # record_dates = Record.objects.filter(entry_date=record.entry_date, habit__user=request.user).order_by('-entry_date')
-    # year = record.entry_date.year
-    # month = record.entry_date.month
-    # day = record.entry_date.day
-
-    # if year is None:
-    #     date_for_record = datetime.date.today()
-    # else:
-    #     date_for_record = datetime.date(year, month, day)
-    # next_day = date_for_record + datetime.timedelta(days=1)
-    # prev_day = date_for_record + datetime.timedelta(days=-1)
-
-    # record = get_object_or_404(Record, pk=pk)
-    # habit = get_object_or_404(Habit, pk=pk)
-    # habit_record, _ = Record.objects.get_or_create(habit=habit, entry_date=datetime.date.today(), result=0)
-
-    # return render(
-    #     request,
-    #     'habit_tracker/test_detail.html',
-    #     {
-    #         "record": record,
-    #         "year": year,
-    #         "month": month,
-    #         "day": day,
-    #         "habit": habit,
-    #         "habit_record": habit_record,
-    #         "next_day": next_day,
-    #         "prev_day": prev_day,
-    #         "date_for_record": date_for_record,
-    #     }
-    # )
+    return render(
+        request,
+        'habit_tracker/test_detail.html',
+        {
+            "habit": habit,
+            "records": records,
+            "date_for_record": date_for_record,
+            "next_day": next_day,
+            "prev_day": prev_day,
+            # "year": year,
+            # "month": month,
+            # "day": day
+            }
+            )
 
 
 @login_required
